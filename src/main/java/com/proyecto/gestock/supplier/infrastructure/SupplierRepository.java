5976cffd74f297d2517738f655d8cb8c06ba8a09
CarloMechatronics <carlo.torres@utec.edu.pe>
2024-06-02 21:37:54 -0500

    Supplier package deleted

--- src/main/java/com/proyecto/gestock/supplier/infrastructure/SupplierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.gestock.supplier.infrastructure;
-
-import com.proyecto.gestock.supplier.domain.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-}
